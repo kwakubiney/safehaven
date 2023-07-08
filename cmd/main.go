8a570fc896817fd8409f09219206347fa252b387
kwakubiney <kebiney@hotmail.com>
2023-07-08 00:22:19 +0000

    create tun interface on client

---
+++ cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/kwakubiney/safehaven/app"
+	"github.com/kwakubiney/safehaven/config"
+)
+
+func main(){
+	var config config.Config
+	flag.StringVar(&config.ClientTunIP, "tunaddr", "192.168.1.100/24", "client tun device ip")
+	flag.StringVar(&config.ServerAddress, "saddr", "", "server address")
+	flag.StringVar(&config.ServerPort, "sport", "", "server port")
+	flag.StringVar(&config.ClientTunName, "tun", "tun0", "tunname")
+	flag.Parse()
+
+	app:= app.NewApp(&config)
+	err := app.StartVPN()
+	if err != nil{
+		log.Fatal(err)
+	}
+}
---
+++ config/config.go
@@ -0,0 +1,8 @@
+package config
+
+type Config struct {
+	ClientTunIP   string
+	ServerAddress string
+	ServerPort    string
+	ClientTunName string
+}
