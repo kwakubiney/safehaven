f8a581f37bcfad5b1eb0ce03bf080ff0bb90893c
kwakubiney <kebiney@hotmail.com>
2023-07-10 00:32:45 +0000

    feat-test: setup client routing (route all traffic to tun)

--- app/app.go
+++ app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"log"
+	"os/exec"
 
 	"github.com/kwakubiney/safehaven/client"
 	"github.com/kwakubiney/safehaven/config"
@@ -39,7 +40,6 @@ func (app *App) StartVPNClient() error {
 	}
 }
 
-
 func (app *App) AssignIPToTun() error {
 	tunLink, err := netlink.LinkByName(app.Config.ClientTunName)
 	if err != nil {
@@ -47,11 +47,39 @@ func (app *App) AssignIPToTun() error {
 	}
 
 	parsedTunIPAddress, err := netlink.ParseAddr(app.Config.ClientTunIP)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
-	netlink.AddrAdd(tunLink, parsedTunIPAddress)
+	err = netlink.AddrAdd(tunLink, parsedTunIPAddress)
+	if err != nil {
+		return err
+	}
+
+	err = netlink.LinkSetUp(tunLink)
+	if err != nil {
+		return err
+	}
+
+	/*
+		I use 0.0.0.0/1 and 128.0.0.0/1 as destination addresses specifically because I want to
+		override the default route without modifying or removing the existing one
+		See: https://serverfault.com/questions/1100250/what-is-the-difference-between-0-0-0-0-0-and-0-0-0-0-1
+		& answer https://serverfault.com/a/1100354
+	*/
+
+	//TODO: Figure out how to use netlink to achieve this
+	routeFirstHalfOfAllDestToTun := exec.Command("ip", "route", "add", "0.0.0.0/1", "dev", app.Config.ClientTunName)
+	_, err = routeFirstHalfOfAllDestToTun.Output()
+	if err != nil {
+		return err
+	}
+
+	routeSecondHalfOfAllDestToTun := exec.Command("ip", "route", "add", "128.0.0.0/1", "dev", app.Config.ClientTunName)
+	_, err = routeSecondHalfOfAllDestToTun.Output()
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
