ce1edcd254bdbfe7ce5ab824f660733b7b576bf4
kb <71296367+kwakubiney@users.noreply.github.com>
2023-07-15 23:41:36 +0000

    feat: udp conn from client to server (#4)

--- config/config.go
+++ config/config.go
@@ -1,9 +1,13 @@
 package config
 
 type Config struct {
-	ClientTunIP   string
-	ServerAddress string
-	ServerPort    string
-	ClientTunName string
-	LocalAddress  string
+	ClientTunIP        string
+	ServerAddress      string
+	ServerPort         string
+	ClientTunName      string
+	LocalAddress       string
+	DestinationAddress string
+	//this to denote routing all traffic
+	Global     bool
+	ServerMode bool
 }
